03ab2902e4780f1dd4ff1f8716b6d017c75d6275
anees555 <aneeshdahal5@gmail.com>
2026-07-07 17:05:42 +0545

    Second phase_part1 : database design and table creation started

---
+++ database/02_create_tables.sql
@@ -0,0 +1,24 @@
+-- create customer table
+CREATE TABLE customers (
+    customer_id VARCHAR(32) PRIMARY KEY,
+    customer_unique_id VARCHAR(32) NOT NULL,
+    customer_zip_code_prefix INTEGER NOT NULL,
+    customer_city VARCHAR(100) NOT NULL,
+    customer_state CHAR(2) NOT NULL
+);
+
+-- create orders table
+CREATE TABLE orders (
+    order_id VARCHAR(32) PRIMARY KEY,
+    customer_id VARCHAR(32) NOT NULL,
+    order_status VARCHAR(20) NOT NULL,
+    order_purchase_timestamp TIMESTAMP NOT NULL,
+    order_approved_at TIMESTAMP,
+    order_delivered_carrier_date TIMESTAMP,
+    order_delivered_customer_date TIMESTAMP,
+    order_estimated_delivery_date TIMESTAMP NOT NULL,
+
+    CONSTRAINT fk_orders_customer
+        FOREIGN KEY (customer_id)
+        REFERENCES customers(customer_id)
+);
