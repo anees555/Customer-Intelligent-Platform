324658445ccdd0b7b6c1080b7aa29af4a127bf75
anees555 <aneeshdahal5@gmail.com>
2026-07-08 17:19:55 +0545

    Schema design completed

--- database/02_create_tables.sql
+++ database/02_create_tables.sql
@@ -22,3 +22,101 @@ CREATE TABLE orders (
         FOREIGN KEY (customer_id)
         REFERENCES customers(customer_id)
 );
+
+-- create product categories table(portuguese -> english translation)
+CREATE TABLE product_categories (
+    product_category_name VARCHAR(100) PRIMARY KEY,
+    category_name_english VARCHAR(100) NOT NULL
+);
+
+-- create product table
+CREATE TABLE products (
+    product_id VARCHAR(32) PRIMARY KEY,
+    product_category_name VARCHAR(100),
+    product_name_length INTEGER,
+    product_description_length INTEGER,
+    product_photos_qty INTEGER,
+    product_weight_g INTEGER,
+    product_length_cm INTEGER,
+    product_height_cm INTEGER,
+    product_width_cm INTEGER,
+
+    CONSTRAINT fk_products_category
+        FOREIGN KEY (product_category_name)
+        REFERENCES product_categories(product_category_name)
+);
+
+-- create order_items table
+CREATE TABLE order_items (
+    order_id VARCHAR(32) NOT NULL,
+    order_item_id INTEGER NOT NULL,
+    product_id VARCHAR(32) NOT NULL,
+    seller_id VARCHAR(32) NOT NULL,
+    shipping_limit_date TIMESTAMP NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
+    freight_value NUMERIC(10, 2) NOT NULL,
+
+    CONSTRAINT pk_order_items
+        PRIMARY KEY (order_id, order_item_id),
+
+    CONSTRAINT fk_order_items_orders
+        FOREIGN KEY (order_id)
+        REFERENCES orders(order_id),
+
+    CONSTRAINT fk_order_items_products
+        FOREIGN KEY (product_id)
+        REFERENCES products(product_id),
+
+    CONSTRAINT fk_order_items_sellers
+        FOREIGN KEY (seller_id)
+        REFERENCES sellers(seller_id)
+);
+
+-- create seller table
+CREATE TABLE sellers (
+    seller_id VARCHAR(32) PRIMARY KEY,
+    seller_zip_code_prefix INTEGER NOT NULL,
+    seller_city VARCHAR(100) NOT NULL,
+    seller_state CHAR(2) NOT NULL
+);
+
+-- create order_payments table
+CREATE TABLE order_payments (
+    order_id VARCHAR(32) NOT NULL,
+    payment_sequential INTEGER NOT NULL,
+    payment_type VARCHAR(30) NOT NULL,
+    payment_installments INTEGER NOT NULL,
+    payment_value NUMERIC(10,2) NOT NULL,
+
+    CONSTRAINT pk_order_payments
+        PRIMARY KEY (order_id, payment_sequential),
+
+    CONSTRAINT fk_order_payments_orders
+        FOREIGN KEY (order_id)
+        REFERENCES orders(order_id)
+);
+
+-- create review table
+CREATE TABLE order_reviews (
+    review_id VARCHAR(32) PRIMARY KEY,
+    order_id VARCHAR(32) UNIQUE NOT NULL,
+    review_score INTEGER NOT NULL,
+    review_comment_title TEXT,
+    review_comment_message TEXT,
+    review_creation_date TIMESTAMP NOT NULL,
+    review_answer_timestamp TIMESTAMP NOT NULL,
+
+    CONSTRAINT fk_order_reviews_orders
+        FOREIGN KEY (order_id)
+        REFERENCES orders(order_id)
+);
+
+-- create geolocation table
+CREATE TABLE geolocation (
+    geolocation_id BIGSERIAL PRIMARY KEY,
+    geolocation_zip_code_prefix INTEGER NOT NULL,
+    geolocation_lat NUMERIC(10,8) NOT NULL,
+    geolocation_lng NUMERIC(11,8) NOT NULL,
+    geolocation_city VARCHAR(100) NOT NULL,
+    geolocation_state CHAR(2) NOT NULL
+);
