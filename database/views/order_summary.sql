e0c6aac9d22c51fb3dcc5e096ecf52c4e1e27fd4
anees555 <aneeshdahal5@gmail.com>
2026-07-12 17:21:00 +0545

    data preparation phase: order summary view created

---
+++ database/views/order_summary.sql
@@ -0,0 +1,54 @@
+----------- ORDER SUMMARY VIEW -----------------
+CREATE OR REPLACE VIEW order_summary AS
+
+WITH item_summary AS (
+    SELECT
+        order_id,
+        COUNT(*) AS total_items,
+        COUNT(DISTINCT seller_id) AS distinct_sellers,
+        SUM(price) AS total_product_price,
+        SUM(freight_value) AS total_freight
+    FROM order_items
+    GROUP BY order_id
+),
+    -- PART B - Payment summary
+    payment_summary AS (
+    SELECT
+        order_id,
+        SUM(payment_value) AS total_payment
+    FROM order_payments
+    GROUP BY order_id
+),
+    -- PART C - review summary
+    review_summary AS (
+    SELECT
+        order_id,
+        ROUND(AVG(review_score), 2) AS review_score
+    FROM order_reviews
+    GROUP BY order_id
+)
+SELECT 
+    o.order_id,
+    o.customer_id,
+    o.order_status,
+    o.order_purchase_timestamp,
+    o.order_approved_at,
+    o.order_delivered_carrier_date,
+    o.order_delivered_customer_date,
+    o.order_estimated_delivery_date,
+	(o.order_delivered_customer_date::date - o.order_purchase_timestamp::date) AS delivery_days,
+	(o.order_delivered_customer_date::date - o.order_estimated_delivery_date::date) AS delivery_delay_days,
+	i.total_items,
+	i.distinct_sellers,
+	i.total_product_price,
+	i.total_freight,
+	p.total_payment,
+	(p.total_payment - i.total_freight) AS product_payment,
+	r.review_score
+FROM orders o
+JOIN item_summary i
+ON o.order_id = i.order_id
+JOIN payment_summary p
+ON o.order_id = p.order_id
+JOIN review_summary r
+ON o.order_id = r.order_id;
---
+++ docs/data_preparation.md
@@ -0,0 +1,14 @@
+# Data Preparation
+
+## Objective
+
+The transactional database is highly normalized, making analytical queries complex and repetitive. During this phase, analytical views are created by aggregating transactional data into denormalized datasets.
+
+The following analytical views will be developed:
+
+- order_summary
+- customer_summary
+- product_summary
+- seller_summary
+
+Each view has a clearly defined grain and is designed for reporting, business intelligence, and machine learning.
