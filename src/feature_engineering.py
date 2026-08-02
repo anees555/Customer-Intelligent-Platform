c96d66b1d30c6163e79c20392e4d179012b1afd1
anees555 <aneeshdahal5@gmail.com>
2026-08-02 09:30:58 +0545

    feature engineering : customer RFM value calculated

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -14,4 +14,25 @@ def customer_features(feature_df):
     # feature 3: Frieght ratio
     feature_df["freight_ratio"] = (feature_df["total_freight_paid"] / feature_df["total_spent"])
 
+    # create recency score
+    feature_df["R_score"] = pd.qcut(feature_df["recency_days"], q=5, labels=[5, 4, 3, 2, 1]).astype(int)
+
+    # create frequency score
+    feature_df["F_score"] = feature_df["total_orders"].apply(
+    lambda x:
+        5 if x >= 5 else
+        4 if x == 4 else
+        3 if x == 3 else
+        2 if x == 2 else
+        1
+    )
+
+    # create monetary score
+    feature_df["M_score"] = pd.qcut(feature_df["total_spent"], q=5, labels=[1, 2, 3, 4, 5]).astype(int)
+
+    # create RFM score
+    feature_df["RFM_score"] = (feature_df["R_score"].astype(str) + feature_df["F_score"].astype(str) + feature_df["M_score"].astype(str))
+
+    
+
     return feature_df
